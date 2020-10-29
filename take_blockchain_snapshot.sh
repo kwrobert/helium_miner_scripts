a5f1be3f8b0284ba8190f97e2ec8f36ec181d7d8
Kyle Robertson <kyle.wesley@me.com>
2020-10-29 21:42:29 +0000

    Added script to take snapshot of blockchain

---
+++ take_blockchain_snapshot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$DATADIR="$HOME/miner_data/"
+
+# DATADIR is bind mounted to /var/data within the container
+snapshot_name="$(ledger_snapshot_$(date +%m-%d-%Y_%Mh%Hm%Ss).bin)"
+docker exec miner_gold miner snapshot take /var/data/$snapshot_name
+
+echo "Snapshot saved to $DATADIR/$snapshot_name
